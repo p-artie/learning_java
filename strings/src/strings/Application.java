825aa9cc98082ac5934befab1ebacee18729f24b
p-artie <artiepolanco@gmail.com>
2022-03-08 12:35:56 -0500

    Learn Java
    Started a Java tutorial. I'm picking up a project at work that is
    built on Java. Picking up the basics and then moving on to
    learn Spring.

---
+++ strings/src/strings/Application.java
@@ -0,0 +1,23 @@
+package strings;
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 7;
+		
+		String text = "Hello";
+		
+		String blank = " ";
+		
+		String name = "Bob";
+		
+		String greeting = text + blank + name;
+		
+		System.out.println(greeting);
+		
+		System.out.println("My integer is: " + myInt);
+		
+		double myDouble = 7.2;
+		
+		System.out.println("My integer is: "+ myDouble + ".");
+	}
+}
