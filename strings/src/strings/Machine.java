654a7686b7b6b576a8e1774a6c25f83caef770ed
p-artie <artiepolanco@gmail.com>
2022-04-08 13:45:07 -0400

    Eclipse will create setters and getters for you!

--- strings/src/strings/Machine.java
+++ strings/src/strings/Machine.java
@@ -5,21 +5,37 @@ public class Machine {
 	private int code;
 	
 	public Machine() {
+		this("Arnie", 0);
 		System.out.println("Constructor running...");
 		
-		name = "Arnie";
 	}
 	
 	public Machine(String name) {
-		
+		this(name, 0);
 		System.out.println("Second constructor running...");
-		this.name = name;
+		this.setName(name);
 	}
 	
 	public Machine(String name, int code) {
-		this.name = name;
-		this.code = code;
+		this.setName(name);
+		this.setCode(code);
 		
 		System.out.println("Third constructor running...");
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
