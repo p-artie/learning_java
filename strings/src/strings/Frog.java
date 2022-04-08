d295f72529f2e79c6eef460a7a6236c561ffac7b
p-artie <artiepolanco@gmail.com>
2022-04-08 13:27:06 -0400

    Setters and This

---
+++ strings/src/strings/Frog.java
@@ -0,0 +1,22 @@
+package strings;
+
+public class Frog {
+	private String name;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public void setAge(int newAge) {
+		age = newAge;
+	}
+}
---
+++ strings/src/strings/TestFrog.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class TestFrog {
+	public static void main(String[] args) {
+		
+		Frog frog1 = new Frog();
+		
+		frog1.setName("Bertie");
+		frog1.setAge(2);
+		
+		System.out.println(frog1.getName());
+		System.out.println(frog1.getAge());
+	}
+}
