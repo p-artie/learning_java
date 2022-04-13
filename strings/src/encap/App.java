e067025d3badcfdf53dcd3a7be57d22d0660db1c
p-artie <artiepolanco@gmail.com>
2022-04-13 17:47:05 -0400

    Learn encapsulation again!

---
+++ strings/src/encap/App.java
@@ -0,0 +1,16 @@
+package encap;
+
+public class App {
+	public static void main(String[] args) {
+		Plant plant = new Plant();
+		plant.setName("Artie");
+		
+		System.out.println(plant.getName());
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ strings/src/encap/Plant.java
@@ -0,0 +1,15 @@
+package encap;
+
+public class Plant {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
