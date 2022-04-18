c0084195c943b7ce33df4091c10d50882d20903f
p-artie <artiepolanco@gmail.com>
2022-04-18 16:35:47 -0400

    Learn exceptions and Child exceptions behavior

---
+++ strings/src/exceptMul/App.java
@@ -0,0 +1,42 @@
+package exceptMul;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class App {
+	public static void main(String[] args) {
+		Test test = new Test();
+		
+		//Catch block for each exception that could be thrown
+		/*
+		try {
+			test.run();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+		
+		//Catch block but with multiple exception in one catch block
+		/*
+		try {
+			test.run();
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+		
+		//Using polymorphism by changing the exception to the parent exception class
+		try {
+			test.run();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	}
+}
