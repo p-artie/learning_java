f85290f65209fc81b27d48999ec5a882fddd3af1
p-artie <artiepolanco@gmail.com>
2022-04-21 17:20:02 -0400

    Learn serialize array

--- strings/src/serial/ReadObjects.java
+++ strings/src/serial/ReadObjects.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class ReadObjects {
 	public static void main(String[] args) {
@@ -14,13 +15,29 @@ public class ReadObjects {
 			
 			ObjectInputStream os = new ObjectInputStream(fi);
 			
-			Person person1 = (Person)os.readObject();
-			Person person2 = (Person)os.readObject();			
+			Person[] people = (Person[])os.readObject();
+			
+			@SuppressWarnings("unchecked")
+			ArrayList<Person> peopleList = (ArrayList<Person>)os.readObject();
+			
+			for(Person person: people) {
+				System.out.println(person);
+			}
+			
+			for(Person person: peopleList) {
+				System.out.println(person);
+			}
+			
+			int num = os.readInt();
+			
+			for(int i=0; i<num; i++) {
+				Person person = (Person)os.readObject();
+				System.out.println(person);
+			}
 			
 			os.close();
 			
-			System.out.println(person1);
-			System.out.println(person2);
+
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
--- strings/src/serial/WriteObjects.java
+++ strings/src/serial/WriteObjects.java
@@ -4,23 +4,30 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WriteObjects {
 	public static void main(String[] args) {
 		System.out.println("Writing objects...");
 		
-		Person mike = new Person(543, "Mike");
-		Person sue = new Person(123, "Sue");
+		Person[] people = {new Person(1, "Sue"), new Person(2, "Artie"), new Person(3, "Lynn")};
 		
-		System.out.println(mike);
-		System.out.println(sue);
+		ArrayList<Person> peopleList = new ArrayList<Person>(Arrays.asList(people));
 		
 		try (FileOutputStream fs = new FileOutputStream("people.bin")) {
 			
 			ObjectOutputStream os = new ObjectOutputStream(fs);
 			
-			os.writeObject(mike);
-			os.writeObject(sue);
+			os.writeObject(people);
+			
+			os.writeObject(peopleList);
+			
+			os.writeInt(peopleList.size());
+			
+			for(Person person: peopleList) {
+				os.writeObject(person);
+			}
 			
 			os.close();
 			
