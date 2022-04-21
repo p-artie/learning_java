bb4d254361bfb37ebf26ee3dc68f12c153b2ca01
p-artie <artiepolanco@gmail.com>
2022-04-21 16:44:19 -0400

    Learn Serializable

---
+++ strings/src/serial/Person.java
@@ -0,0 +1,23 @@
+package serial;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8666152311985535822L;
+	private int id;
+	private String name;
+	
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [id = " + id + ", name = " + name + "]";
+	}
+}
