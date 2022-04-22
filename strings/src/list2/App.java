2aa638ed749196fc8962911e879874c8a85a10dd
p-artie <artiepolanco@gmail.com>
2022-04-22 16:44:54 -0400

    Learn Linked lists speed vs arraylist when retreiving items in set indexes

---
+++ strings/src/list2/App.java
@@ -0,0 +1,44 @@
+package list2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class App {
+
+	public static void main(String[] args) {
+		List<Integer> arrayList = new ArrayList<Integer>();
+		List<Integer> linkedList = new LinkedList<Integer>();
+		
+		doTimings("ArrayList", arrayList);
+		doTimings("LinkedList", linkedList);
+
+	}
+
+	private static void doTimings(String type, List<Integer> list) {
+		
+		for(int i=0; i<1E5; i++) {
+			list.add(i);
+		}
+		
+		long start = System.currentTimeMillis();
+		
+		/*
+		// Adding items at end of list
+		for(int i = 0; i<1E5; i++) {
+			list.add(i);
+		}
+		*/
+		
+		//Add items elsewhere in list
+		for(int i=0; i<1E5; i++) {
+			list.add(list.size()-5000, i);
+
+		}
+		
+		long end = System.currentTimeMillis();
+		
+		System.out.println("Time taken: " + (end - start) + " ms for " + type);
+	}
+	
+}
